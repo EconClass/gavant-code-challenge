c1737eec6cf8702d1147482f52b66aefeb15210a
Zurich Okoren <zurich.okoren@gmail.com>
2021-03-18 23:36:25 -0400

    [add] dasboard and overview skeletons

--- app/controllers/customers/index.js
+++ app/controllers/customers/index.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { sort } from '@ember/object/computed';
+import { sort, filter } from '@ember/object/computed';
 import { tracked } from '@glimmer/tracking';
 
 export default class CustomersIndexController extends Controller {
@@ -9,9 +9,16 @@ export default class CustomersIndexController extends Controller {
   @sort('model', 'customersSortProps')
   sortedCustomers;
 
+  // @filter('model', 'customersFilterProps')
+  // filteredCustomers;
+
   get customersSortProps() {
     return [this.sortProperty];
   }
+
+  // get customersFilterProps (customer, index, array) {
+  //   return customer
+  // }
 
   @action
   updateSortProperty(event) {

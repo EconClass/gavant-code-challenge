45e41f2200b4ca8cb8c52cb1dba1b37440561d08
Zurich Okoren <zurich.okoren@gmail.com>
2021-03-20 09:57:47 -0400

    [add] search bar

--- app/controllers/customers/index.js
+++ app/controllers/customers/index.js
@@ -1,20 +1,35 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { sort } from '@ember/object/computed';
+import { sort, filter } from '@ember/object/computed';
 import { tracked } from '@glimmer/tracking';
 
 export default class CustomersIndexController extends Controller {
   @tracked sortProperty = 'lastName';
+  @tracked keyword = '';
 
   @sort('model', 'customersSortProps')
   sortedCustomers;
 
+  @filter('sortedCustomers', (customer) => {
+    const {lastName, project, company} = customer
+    if(lastName == this.keyword || project == this.keyword || company == this.keyword) {
+      return customer
+    }
+  })
+  filteredCustomers;
+
+
   get customersSortProps() {
-    return [this.sortProperty];
+    return [this.sortProperty]
   }
 
   @action
   updateSortProperty(event) {
     this.sortProperty = event.target.value
+  }
+
+  @action
+  searchByKeyword(event) {
+    this.keyword = event.target.value
   }
 }
